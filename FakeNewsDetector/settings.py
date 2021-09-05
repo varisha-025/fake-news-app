ec89de165a67701bce9d3fe350a44f00067edad1
varru15 <flashaquas@gmail.com>
2021-09-05 22:54:19 +0530

    updated settings.py

--- FakeNewsDetector/settings.py
+++ FakeNewsDetector/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static"
 # ]
+django_heroku.settings(locals())
